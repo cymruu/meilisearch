7728ae13d195d7d3a1972a70c1f492b74e331871
Filip Bachul <filipbachul@gmail.com>
2024-06-06 00:34:26 +0200

    chore: ActionFlags

--- meilisearch-types/src/actions.rs
+++ meilisearch-types/src/actions.rs
@@ -1,7 +1,9 @@
+use std::hash::Hash;
+
+use bitflags::bitflags;
 use deserr::Deserr;
 use enum_iterator::Sequence;
 use serde::{Deserialize, Serialize};
-use std::hash::Hash;
 
 #[derive(Copy, Clone, Serialize, Deserialize, Debug, Eq, PartialEq, Hash, Sequence, Deserr)]
 #[repr(u8)]
@@ -192,4 +194,48 @@ pub mod actions {
     pub const KEYS_DELETE: u8 = KeysDelete.repr();
     pub const EXPERIMENTAL_FEATURES_GET: u8 = ExperimentalFeaturesGet.repr();
     pub const EXPERIMENTAL_FEATURES_UPDATE: u8 = ExperimentalFeaturesUpdate.repr();
+}
+
+#[derive(Copy, Clone, Serialize, Deserialize, Debug, Eq, PartialEq, Hash)]
+pub struct ActionFlags(u64);
+
+bitflags! {
+    impl ActionFlags:u64{
+        const Search = 1<<0;
+        const DocumentsAll = 1 << 1;
+        const DocumentsAdd = 1 << 2;
+        const DocumentsGet = 1 << 3;
+        const DocumentsDelete = 1 << 4;
+        const IndexesAll = 1 << 5;
+        const IndexesAdd = 1 << 6;
+        const IndexesGet = 1 << 7;
+        const IndexesUpdate = 1 << 8;
+        const IndexesDelete = 1 << 9;
+        const IndexesSwap = 1 << 10;
+        const TasksAll = 1 << 11;
+        const TasksCancel = 1 << 12;
+        const TasksDelete = 1 << 13;
+        const TasksGet = 1 << 15;
+        const SettingsAll = 1 << 16;
+        const SettingsGet = 1 << 17;
+        const SettingsUpdate = 1 << 18;
+        const StatsAll = 1 << 19;
+        const StatsGet = 1 << 20;
+        const MetricsAll = 1 << 21;
+        const MetricsGet = 1 << 22;
+        const DumpsAll = 1 << 23;
+        const DumpsCreate = 1 << 24;
+        const SnapshotsAll = 1 << 25;
+        const SnapshotsCreate = 1 << 26;
+        const Version = 1 << 27;
+        const KeysAdd = 1 << 28;
+        const KeysGet = 1 << 29;
+        const KeysUpdate = 1 << 30;
+        const KeysDelete = 1 << 31;
+        const ExperimentalFeaturesGet = 1 << 32;
+        const ExperimentalFeaturesUpdate = 1 << 33;
+
+        const _ = !0;
+        const All = 1 << 63;
+    }
 }
